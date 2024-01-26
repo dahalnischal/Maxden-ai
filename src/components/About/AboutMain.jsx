4876c49bd6882fd8e24bae296efe7114b56b67bb
dahalnischal <dahalnischal2057@gmail.com>
2024-01-26 10:20:53 +0545

    made change in popup added parallex effect to home page and completed half of the about page

---
+++ src/components/About/AboutMain.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const AboutMain = () => {
+  return (
+    <section className="about-main">
+      <div className="about-wraps">
+        <div className="about-heading">
+          <h1>maxden.ai - Ваш Надежный Партнер в Мире ИИ</h1>
+        </div>
+        <div className="counter-wraps">
+          <div className="value-wraps">
+            <h1>4+</h1>
+            <span>лет опыта работы</span>
+          </div>
+          <div className="value-wraps">
+            <h1>12</h1>
+            <span>человек в команде</span>
+          </div>
+          <div className="value-wraps">
+            <h1>20+</h1>
+            <span>реализованных проектов </span>
+          </div>
+          <div className="value-wraps">
+            <h1>1</h1>
+            <span>млн. нагруженнных запросов в день</span>
+          </div>
+          <div className="value-wraps">
+            <h1>100</h1>
+            <span>тыс. чашек кофе выпито</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutMain;
---
+++ src/components/Dialog/Neuro/Neuro4.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+const Neuro4 = ({ setNeuro4 }) => {
+  return (
+    <section className="project-pop">
+      <div className="project-popup">
+        <div className="project-pop-wraps">
+          <div className="cross" onClick={() => setNeuro4(false)}>
+            <img src="assets/images/Burger.png" alt="cross" />
+          </div>
+          <div className="scroll-wraps">
+            <div className="project-content-wraps">
+              <div className="topic">
+                <span>Услуга: Автоматизация Аналитики и Отчетности</span>
+                <h2>Neuro 4</h2>
+              </div>
+              <div>
+                <img src="assets/images/metrica.png" alt="metrica" />
+              </div>
+              <div className="listed-paragraph">
+                <div>
+                  <span>Цель:</span>
+                  <ul>
+                    <li>
+                      <span>Улучшение пользовательского опыта</span> за счет
+                      устранения пропусков в данных
+                    </li>
+                  </ul>
+                </div>
+                <div className="mt-25">
+                  <span>Результат:</span>
+                  <ul>
+                    <li>
+                      <span>на 85%</span> снизили количество пропусков в данных
+                    </li>
+                    <li>
+                      повысили общее удовлетворение пользователей{" "}
+                      <span>с 3-х до 5-ти звезд</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="project-paragraph">
+                <p>
+                  Сервис metrica.guru агрегирует аналитические данные о сайтах.
+                  Проблема компании заключалась в негативном влиянии пропусков
+                  данных на пользовательский опыт.&nbsp;
+                  <br />
+                  Создали четыре веб-сервиса для прогнозирования недостающих
+                  данных о трафике сайтов и эффективности рекламных кампаний.
+                  Разработка включала использование нейронных сетей для анализа
+                  и предсказания данных с точностью от 95%, а также интеграцию с
+                  платформой metrica.guru.
+                </p>
+              </div>
+            </div>
+            <div className="btns">
+              <a href className="btn-bdr">
+                Вернуться на сайт
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Neuro4;
